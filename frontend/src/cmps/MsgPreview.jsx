9a7ede90b5f0875f4f9c4ae5849132881905ed2e
Dafna Bashan <dafnaba1@gmail.com>
2023-09-12 12:55:24 +0300

    adding triangle to msg preview

--- frontend/src/cmps/MsgPreview.jsx
+++ frontend/src/cmps/MsgPreview.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 export function MsgPreview({ msg, loggedInUser, isGroupChat }) {
   return (
     <div className={loggedInUser._id === msg.from._id ? "msg-preview me" : "msg-preview"}>
-      <div className={loggedInUser._id === msg.from._id ? "me" : "other"}>
-        <div className="name">{msg.from.firstName}</div>
+      <div className={loggedInUser._id === msg.from._id ? "txt me" : "txt other"}>
+        {loggedInUser._id !== msg.from._id && <div className="name">{msg.from.firstName}</div>}
         <div className="msg">{msg.txt}</div>
       </div>
+      <span className={loggedInUser._id === msg.from._id ? "triangle me" : "triangle other"}></span>
       {isGroupChat && loggedInUser._id !== msg.from._id && <img src={msg.from.imgUrl} alt={msg.from.firstName} />}
     </div>
   )
